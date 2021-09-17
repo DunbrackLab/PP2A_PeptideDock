cebb85d5167e3a75155e5d0b0c548edf7706bbf1
Qifang <qifang.xu@fccc.edu>
2021-09-17 11:17:46 -0400

    Add files via upload

---
+++ PeptideDockingLib/MatrixOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeptideDockingLib
+{
+    public class MatrixOperations
+    {
+        public static T[] GetColumn<T>(T[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x, columnNumber])
+                    .ToArray();
+        }
+
+        public static T[] GetRow<T>(T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => matrix[rowNumber, x])
+                    .ToArray();
+        }
+    }
+}
---
+++ PeptideDockingLib/MotifSeqInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeptideDockingLib
+{
+    public class MotifSeqInfo
+    {
+        public string motif;   // sequence
+        public string chainId;
+        public int motifBeg;  // this is the sequential residue number 1...N
+    }
+}
